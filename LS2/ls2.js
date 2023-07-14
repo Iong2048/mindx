2b79e144b78bf243f4a9afcc6163e9d1f1490095
hoang long <lainguyenhoanglong.mxt@mgial.com>
2023-07-14 21:28:35 +0700

    first commit

---
+++ LS2/ls2.js
@@ -0,0 +1,18 @@
+for(let i=0;i<=5;i++){
+    console.log('hi');
+}
+let n=0;
+while(n<=5){
+    console.log('hi');
+    n++;
+}
+let m=0;
+do{
+    console.log('hi');
+    m++;
+}
+while(m<=5);
+let y= ['Long 1', 'Long 2', 'Long 3'];
+for(let i=0; i<y.length;i++){
+    console.log(y[i]);
+}
